9f81c7aa9ddafebbe45d0304dcaa916dc7344641
larrivain <daphne.larrivain@ecole.ensicaen.fr>
2025-07-16 14:19:00 +0200

    magenta fix

--- tulipviz/layer_svg.py
+++ tulipviz/layer_svg.py
@@ -8,6 +8,17 @@ def is_white_group(group, ns):
             return True
     return False
 
+def is_magenta_group(group, ns):
+    rects = group.xpath('.//svg:rect', namespaces={'svg': ns})
+    for rect in rects:
+        fill = rect.attrib.get('fill', '').lower()
+        if '255,192,255' in fill:
+            return True
+    return False
+
+def is_relevant_group(group, ns):
+    return is_white_group(group, ns) or is_magenta_group(group, ns)
+
 def push_white_groups_to_end(svg_path, output_path=None):
     parser = ET.XMLParser(remove_blank_text=True)
     tree = ET.parse(svg_path, parser)
@@ -23,7 +34,7 @@ def push_white_groups_to_end(svg_path, output_path=None):
         parent = group.getparent()
         if parent is None:
             continue
-        if is_white_group(group, ns):
+        if is_relevant_group(group, ns):
             white_groups.append((group, parent))
         else:
             non_white_groups.append((group, parent))
